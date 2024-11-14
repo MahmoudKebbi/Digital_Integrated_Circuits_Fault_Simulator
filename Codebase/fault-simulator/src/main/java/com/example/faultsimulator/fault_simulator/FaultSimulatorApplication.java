73a18d213e8876878d5e28cefb5550afede57d89
MahmoudKebbi <mahmoud.kebbi180@gmail.com>
2024-11-14 15:24:10 +0300

    Initialized Spring Boot Project

---
+++ Codebase/fault-simulator/src/main/java/com/example/faultsimulator/fault_simulator/FaultSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.faultsimulator.fault_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaultSimulatorApplication.class, args);
+	}
+
+}
---
+++ Codebase/fault-simulator/src/test/java/com/example/faultsimulator/fault_simulator/FaultSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faultsimulator.fault_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
